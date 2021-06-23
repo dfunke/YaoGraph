2e800aac4b24a925ecd539a0103168bb2417901c
Daniel Funke <daniel.funke@kit.edu>
2021-06-23 12:46:42 +0200

    Add GTest

---
+++ test/SweepLineTest.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include "SweepLine.hpp"
+#include "Generators.hpp"
+
+TEST(SweepLineTest, Ordering) { // 12/2/2020 -> 737761
+    constexpr tBox BOUNDS{{0, 0},
+                          {1, 1}};
+    constexpr tIndex nPoints = 10;
+    constexpr tDim K = 6;
+
+
+    Uniform uni;
+    auto points = uni.generate(nPoints, BOUNDS);
+
+    SweepLine<K> sl;
+    sl(points);
+}
