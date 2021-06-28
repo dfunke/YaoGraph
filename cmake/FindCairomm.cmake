b6ef806e2eaa79ad499370ef8d13ba94a6022137
Daniel Funke <daniel.funke@kit.edu>
2021-06-28 22:36:52 +0200

    make FindCairomm and FindSigC++ more general

--- cmake/FindCairomm.cmake
+++ cmake/FindCairomm.cmake
@@ -11,23 +11,39 @@ include(LibFindMacros)
 libfind_package(Cairomm Cairo)
 libfind_package(Cairomm SigC++)
 
+foreach (dir ${CMAKE_SYSTEM_PREFIX_PATH})
+    #    message(INFO ${dir})
+    file(GLOB Cairomm_VERSION_PATH "${dir}/include/cairomm*")
+    #    message(INFO ${Cairomm_VERSION_PATH})
+    if (NOT ${Cairomm_VERSION_PATH} STREQUAL "")
+        break()
+    endif ()
+endforeach ()
+
+#message(INFO ${Cairomm_VERSION_PATH})
+get_filename_component(Cairomm_VERSION_DIR ${Cairomm_VERSION_PATH} NAME)
+#message(INFO ${Cairomm_VERSION_DIR})
+string(REPLACE "cairomm-" "" Cairomm_VERSION ${Cairomm_VERSION_DIR})
+#message(INFO ${Cairomm_VERSION})
+
+
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(Cairomm_PKGCONF cairomm-1.16)
+libfind_pkg_check_modules(Cairomm_PKGCONF ${Cairomm_VERSION_DIR})
 
 # Main include dir
 find_path(Cairomm_INCLUDE_DIR
-  NAMES cairomm/cairomm.h
-  PATHS ${Cairomm_PKGCONF_INCLUDE_DIRS}
-  PATH_SUFFIXES cairomm-1.16
-)
+        NAMES cairomm/cairomm.h
+        PATHS ${Cairomm_PKGCONF_INCLUDE_DIRS}
+        PATH_SUFFIXES ${Cairomm_VERSION_DIR}
+        )
 
 find_path(Cairommconfig_INCLUDE_DIR
-  NAMES cairommconfig.h
-  PATHS ${Cairomm_PKGCONF_INCLUDE_DIRS}
-  PATH_SUFFIXES cairomm-1.16
-)
+        NAMES cairommconfig.h
+        PATHS ${Cairomm_PKGCONF_INCLUDE_DIRS}
+        PATH_SUFFIXES ${Cairomm_VERSION_DIR}
+        )
 
-libfind_library(Cairomm cairomm 1.16)
+libfind_library(Cairomm cairomm ${Cairomm_VERSION})
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
--- cmake/FindSigC++.cmake
+++ cmake/FindSigC++.cmake
@@ -7,24 +7,39 @@
 
 include(LibFindMacros)
 
+foreach (dir ${CMAKE_SYSTEM_PREFIX_PATH})
+    #    message(INFO ${dir})
+    file(GLOB SigC++_VERSION_PATH "${dir}/include/sigc++*")
+    #    message(INFO ${SigC++_VERSION_PATH})
+    if (NOT ${SigC++_VERSION_PATH} STREQUAL "")
+        break()
+    endif ()
+endforeach ()
+
+#message(INFO ${SigC++_VERSION_PATH})
+get_filename_component(SigC++_VERSION_DIR ${SigC++_VERSION_PATH} NAME)
+#message(INFO ${SigC++_VERSION_DIR})
+string(REPLACE "sigc++-" "" SigC++_VERSION ${SigC++_VERSION_DIR})
+#message(INFO ${SigC++_VERSION})
+
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(SigC++_PKGCONF sigc++-3.0)
+libfind_pkg_check_modules(SigC++_PKGCONF ${SigC++_VERSION_DIR})
 
 # Main include dir
 find_path(SigC++_INCLUDE_DIR
-  NAMES sigc++/sigc++.h
-  PATHS ${SigC++_PKGCONF_INCLUDE_DIRS}
-  PATH_SUFFIXES sigc++-3.0
-)
+        NAMES sigc++/sigc++.h
+        PATHS ${SigC++_PKGCONF_INCLUDE_DIRS}
+        PATH_SUFFIXES ${SigC++_VERSION_DIR}
+        )
 
 # Glib-related libraries also use a separate config header, which is in lib dir
 find_path(SigC++Config_INCLUDE_DIR
-  NAMES sigc++config.h
-  PATHS ${SigC++_PKGCONF_INCLUDE_DIRS} /usr
-  PATH_SUFFIXES lib/sigc++-3.0/include
-)
+        NAMES sigc++config.h
+        PATHS ${SigC++_PKGCONF_INCLUDE_DIRS} /usr
+        PATH_SUFFIXES lib/${SigC++_VERSION_DIR}/include
+        )
 
-libfind_library(SigC++ sigc 3.0)
+libfind_library(SigC++ sigc ${SigC++_VERSION})
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
