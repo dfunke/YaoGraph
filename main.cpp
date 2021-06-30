72533fce32bf04d6f544dbbd6b7c7b4593e0ce80
Daniel Funke <daniel.funke@kit.edu>
2021-06-30 10:09:26 +0200

    cleanup main

--- main.cpp
+++ main.cpp
@@ -38,10 +38,12 @@ int main() {
         auto points = uni.generate(nPoints, BOUNDS);
 
         auto naive = Timer<NaiveYao<Cones>>::time(points);
-        auto grid = Timer<GridYao<Cones>>::time(points, BOUNDS, cellOcc);
-        auto sl = Timer<SweepLine<Cones>>::time(points, BOUNDS);
 
+        auto grid = Timer<GridYao<Cones>>::time(points, BOUNDS, cellOcc);
         checkGraph(std::get<1>(grid), std::get<1>(naive));
+
+        auto sl = Timer<SweepLine<Cones>>::time(points, BOUNDS);
+        checkGraph(std::get<1>(sl), std::get<1>(naive));
 
 #ifdef WITH_CAIRO
         Painter basePainter(BOUNDS, 1000);
@@ -55,41 +57,6 @@ int main() {
         Painter gridPainter(basePainter);
         gridPainter.draw(std::get<1>(grid), points);
         gridPainter.save("03_grid");
-
-        tIndex i = 71;
-        auto exp = std::get<1>(naive)[i];
-        auto is = std::get<1>(grid)[i];
-        Painter detailPainter(basePainter);
-        detailPainter.drawCones(points[i], Cones);
-
-        detailPainter.setColor(.5, .5, .5, .5);
-        detailPainter.drawGrid(25 * 25);
-
-        detailPainter.setColor(1, 0, 0);
-        detailPainter.draw(i, exp, points);
-
-        detailPainter.setColor(0, 0, 0);
-        detailPainter.draw(i, is, points);
-        detailPainter.save("04_detail");
-
-        std::cout << "is:  " << is << std::endl;
-        std::cout << "exp: " << exp << std::endl;
-        for (tDim k = 0; k < Cones; ++k) {
-            auto nis = is.neighbor[k];
-            auto nexp = exp.neighbor[k];
-            std::cout << "\t" << i << "-" << nis;
-            if (nis != tIndex(-1)) {
-                std::cout << ": " << atan2P(points[i], points[nis]) << " rad, sector "
-                          << std::floor(atan2P(points[i], points[nis]) / (2 * M_PI / Cones))
-                          << " dist " << is.distance[k];
-            }
-            if (nexp != nis && nexp != tIndex(-1)) {
-                std::cout << " EXP : " << atan2P(points[i], points[nexp]) << " rad, sector "
-                          << std::floor(atan2P(points[i], points[nexp]) / (2 * M_PI / Cones))
-                          << " dist " << exp.distance[k];
-            }
-            std::cout << std::endl;
-        }
 #endif
 
 #ifdef WITH_CGAL
