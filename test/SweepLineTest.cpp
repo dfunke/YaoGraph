04a3ecd921a0005fbec3dfb03c98065255b47017
Daniel Funke <funke@kit.edu>
2022-05-06 16:36:20 +0200

    set output precision

--- test/SweepLineTest.cpp
+++ test/SweepLineTest.cpp
@@ -16,6 +16,8 @@ TEST(SweepLineTest, Ordering) {
     Uniform uni;
     auto points = uni.generate(nPoints, BOUNDS);
 
+    std::cout << std::setprecision(std::numeric_limits<long double>::digits10 + 1);
+
     SweepLine<K, CGALKernel<ExactPredicatesInexactConstructions>> sl;
     auto is = sl(points, BOUNDS);
 
