509f5635eea502efe448e4aafc8f7b7a5998ff28
Daniel Funke <funke@kit.edu>
2022-05-18 15:57:34 +0200

    exact GridYao

--- include/GridYao.hpp
+++ include/GridYao.hpp
@@ -8,17 +8,20 @@
 #include "Types.hpp"
 #include "utils/InexactKernel.hpp"
 
-template<tDim K, typename Kernel, tIndex CellOcc>
+template<tDim C, typename Kernel, tIndex CellOcc>
 class GridYao {
 
 private:
     using tEFloat = typename Kernel::Float;
     using tEFloatVector = std::array<tEFloat, D>;
 
+    using tKPoint = typename Kernel::Point;
+    using tKPoints = std::vector<tKPoint>;
+
     class Grid {
 
     private:
-        friend class GridYao<K, Kernel, CellOcc>;
+        friend class GridYao<C, Kernel, CellOcc>;
 
         using tGridCell = std::vector<tIndex>;
         using tGrid = std::vector<tGridCell>;
@@ -31,21 +34,22 @@ private:
             for (tDim d = 0; d < bounds.high.size(); ++d) {
                 cellSize[d] = (bounds.high[d] - bounds.low[d]) / numCellsPerDim;
             }
+            minCellSize = *std::min_element(cellSize.begin(), cellSize.end());
 
             cells.resize(numCells);
         }
 
-        void buildGrid(const tPoints &points) {
+        void buildGrid(const tKPoints &points) {
             for (tIndex i = 0; i < points.size(); ++i) {
                 tIndex idx = getIndex(points[i]);
                 cells[idx].push_back(i);
             }
         }
 
-        tIndexVector getIndexVector(const tIFloatVector &p) const {
+        tIndexVector getIndexVector(const tKPoint &p) const {
             tIndexVector idx;
             for (tDim d = 0; d < idx.size(); ++d) {
-                idx[d] = std::floor(p[d] / cellSize[d]);
+                idx[d] = std::floor(Kernel::to_float(p[d] / cellSize[d]));//TODO exact?
             }
             return idx;
         }
@@ -58,7 +62,7 @@ private:
             return i;
         }
 
-        tIndex getIndex(const tIFloatVector &p) const {
+        tIndex getIndex(const tKPoint &p) const {
             return getIndex(getIndexVector(p));
         }
 
@@ -81,29 +85,36 @@ private:
     private:
         tIndex numCellsPerDim;
         tIndex numCells;
-        tIFloatVector cellSize;
+        tEFloatVector cellSize;
+        tEFloat minCellSize;
         tBox bounds;
         tGrid cells;
     };
 
 public:
-    using tVertex = tYaoVertex<K, tEFloat>;
+    using tVertex = tYaoVertex<C, tEFloat>;
     using tGraph = tYaoGraph<tVertex>;
 
     static std::string name() {
         return "GridYao" + std::to_string(CellOcc) + "_" + Kernel::name();
     }
 
-    tGraph operator()(const tPoints &points, const tBox &bounds) const {
-        tGraph graph(points.size());
-        Grid grid(bounds, std::ceil(points.size() / static_cast<tIFloat>(CellOcc)));
-        grid.buildGrid(points);
+    tGraph operator()(const tPoints &iPoints, const tBox &bounds) const {
+        tGraph graph(iPoints.size());
 
-        for (tIndex i = 0; i < points.size(); ++i) {
+        auto rays = Kernel::computeCones(C);
+        auto kPoints = Kernel::mkPoints(iPoints);
+
+        Grid grid(bounds, std::ceil(kPoints.size() / static_cast<tIFloat>(CellOcc)));
+        grid.buildGrid(kPoints);
+
+        for (tIndex i = 0; i < kPoints.size(); ++i) {
+
+            auto cLines = Kernel::computePointCones(kPoints[i], rays);
 
             for (tIndex radius = 0;
-                 !isFinalized(graph[i], radius, grid.cellSize) && radius < grid.numCellsPerDim; ++radius) {
-                searchRadius(i, radius, graph, grid, points);
+                 !isFinalized(graph[i], radius, grid.minCellSize) && radius < grid.numCellsPerDim; ++radius) {
+                searchRadius(i, radius, graph, grid, kPoints, cLines);
             }
         }
 
@@ -111,17 +122,18 @@ public:
     }
 
 private:
-    bool isFinalized(const tVertex &v, const tIndex &radius, const tIFloatVector &cellSize) const {
+    bool isFinalized(const tVertex &v, const tIndex &radius, const tEFloat &minCellSize) const {
 
-        auto minLength = *std::min_element(cellSize.begin(), cellSize.end());
+        auto d = static_cast<int>(radius - 1) * minCellSize;//TODO better type handling, exact computation;
+        auto d2 = d * d;
 
         for (tDim k = 0; k < v.neighbor.size(); ++k) {
-            if (v.neighbor[k] == tIndex(-1)) {
+            if (v.neighbor[k] == INF_IDX) {
                 // no neighbor found so far
                 return false;
             } else {
                 // we found a neighbor for given search radius, can we find a closer one still
-                if (v.distance[k] > std::pow((radius - 1) * minLength, 2)) {//TODO exact calculation
+                if (v.distance[k] > d2) {
                     // found neighbor is further away than any point for next cell radius could be
                     return false;
                 }
@@ -132,7 +144,7 @@ private:
     }
 
     void searchRadius(const tIndex &point, const int &radius,
-                      tGraph &graph, const Grid &grid, const tPoints &points) const {
+                      tGraph &graph, const Grid &grid, const tKPoints &points, const auto &cLines) const {
 
         auto idx = grid.getIndexVector(points[point]);
 
@@ -147,14 +159,14 @@ private:
                 auto cell = idx + std::array<int, idx.size()>({dx, dy});
                 if (std::all_of(cell.begin(), cell.end(),
                                 [&grid](const auto &x) { return x < grid.numCellsPerDim; })) {
-                    visitGridCell(point, cell, graph, grid, points);
+                    visitGridCell(point, cell, graph, grid, points, cLines);
                 }
             }
         }
     }
 
     void visitGridCell(const tIndex &point, const tIndexVector &cell,
-                       tGraph &graph, const Grid &grid, const tPoints &points) const {
+                       tGraph &graph, const Grid &grid, const tKPoints &points, const auto &cLines) const {
 
         auto &vertex = graph[point];
 
@@ -162,12 +174,11 @@ private:
 
             if (p == point) continue;
 
-            auto d = Kernel::distance2(Kernel::mkPoint(points[point]), Kernel::mkPoint(points[p]));
-            tDim sec = std::floor(atan2P(points[point], points[p]) / (2 * M_PI / K));
+            auto sec = Kernel::getCone(points[point], points[p], cLines);
 
-            if (d < vertex.distance[sec]) {
+            if (vertex.neighbor[sec] == INF_IDX || Kernel::compareDistance(points[point], points[p], points[vertex.neighbor[sec]], vertex.distance[sec])) {
                 vertex.neighbor[sec] = p;
-                vertex.distance[sec] = d;//TODO: exact solution
+                vertex.distance[sec] = Kernel::distance2(points[point], points[p]);
             }
         }
     }
