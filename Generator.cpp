1cfb6a55cf1d673a965550177dfda2e77fd4af2b
Daniel Funke <funke@kit.edu>
2023-02-02 11:30:51 +0100

    output

--- Generator.cpp
+++ Generator.cpp
@@ -24,7 +24,7 @@ const char Dists[] = {'u', 'g', 'd', 'r', 's', 'c', 'b'};
 
 void generate(GeneratorBase &gen, const tIndex &n, const tBox &iBounds) {
 
-    std::cout << "Generating " << gen.name() << " with " << n << " points and seed " << gen.seed() << std::endl;
+    std::cout << "Generating " << gen.name() << " with " << n << " points and seed " << gen.seed() << ": " << std::flush;
 
     auto [points, oBounds] = gen.generate(n, iBounds);
     ASSERT(points.size() >= n);
@@ -44,6 +44,8 @@ void generate(GeneratorBase &gen, const tIndex &n, const tBox &iBounds) {
     for (auto &p : points) {
         file << p[X] << " " << p[Y] << std::endl;
     }
+
+    std::cout << points.size() << "generated" << std::endl;
 }
 
 int main(int argc, char **argv) {
